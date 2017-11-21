a00240f4a564e7a2d21259fe5f1e2de83d98f7cc
prabhatag <prabhat.ag@gmail.com>
2017-11-21 23:34:30 +0530

    sql4

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var con = mysql.createConnection({
 con.connect( function(err){
     if (err) throw err;
     console.log("Connected!");
+	con.query("SELECT * FROM users", function(err,result,fields){
+		console.log(result);
+	}
 });
 app.use(session({secret: 'randomsecret'}));
 app.use(bodyParser.json());
